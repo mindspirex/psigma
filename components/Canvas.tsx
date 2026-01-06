6e857bdd233ae72df469d93991703e87d90edcca
Sanskar Kumar <mindspirex@gmail.com>
2026-01-06 18:29:20 +0530

    now it works

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,21 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Obj } from "@/context/ObjectsContext";
+import { useObjects } from "@/context/ObjectsContext";
 import RenderObject from "@/components/RenderObject";
 
 export default function Canvas() {
-  const [objects, setObjects] = useState<Obj[]>([]);
-
-  useEffect(() => {
-    async function load() {
-      const res = await fetch("/api/objects");
-      const data = await res.json();
-      setObjects(data);
-    }
-
-    load();
-  }, []);
+  const { objects } = useObjects();
 
   return (
     <div className="canvas h-screen">
