e1e6310295b7ef492de2d6995c63f73d455926ac
Sanskar Kumar <mindspirex@gmail.com>
2026-01-23 21:05:13 +0530

    post route

--- app/api/projects/route.tsx
+++ app/api/projects/route.tsx
@@ -1,17 +1,14 @@
-import { getAuthPayload } from "@/utility/auth";
+import { getAuthUser } from "@/utility/auth";
 import { ProjectModel, UserModel } from "@/db/schema";
 import dbConnect from "@/db/mongodb";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const decoded = await getAuthPayload();
+    // get user if user if logged in
+    const user = await getAuthUser();
 
     await dbConnect();
-
-    const user = await UserModel.findOne({ email: decoded.email });
-    if (!user) {
-      return Response.json({ message: "User not found" }, { status: 404 });
-    }
 
     const projects = await ProjectModel.find({
       ownerId: user._id,
@@ -20,6 +17,35 @@ export async function GET() {
     return Response.json(projects, { status: 200 });
   } catch (error) {
     console.error(error);
-    return Response.json({ message: "Unauthorized" }, { status: 401 });
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    // get user if user is logged in
+    const user = await getAuthUser();
+
+    await dbConnect();
+
+    const body = await request.json();
+    const { name } = body;
+
+    if (!name) {
+      return Response.json(
+        { message: "Project name is required" },
+        { status: 400 },
+      );
+    }
+
+    const project = await ProjectModel.create({
+      name,
+      ownerId: user._id,
+    });
+
+    return NextResponse.json(project, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 }
--- utility/auth.ts
+++ utility/auth.ts
@@ -1,11 +1,13 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { cookies } from "next/headers";
+import dbConnect from "@/db/mongodb";
+import { UserModel } from "@/db/schema";
 
 export interface AuthPayload extends JwtPayload {
   email: string;
 }
 
-export async function getAuthPayload(): Promise<AuthPayload> {
+export async function getAuthUser() {
   const cookieStore = await cookies();
   const token = cookieStore.get("access_token")?.value;
 
@@ -18,5 +20,21 @@ export async function getAuthPayload(): Promise<AuthPayload> {
     throw new Error("JWT_SECRET_MISSING");
   }
 
-  return jwt.verify(token, JWT_SECRET) as AuthPayload;
+  let decoded: AuthPayload;
+
+  try {
+    decoded = jwt.verify(token, JWT_SECRET) as AuthPayload;
+  } catch (error) {
+    console.log(error);
+    throw new Error("INVALID_TOKEN");
+  }
+
+  await dbConnect();
+
+  const user = await UserModel.findOne({ email: decoded.email });
+  if (!user) {
+    throw new Error("USER_NOT_FOUND");
+  }
+
+  return user;
 }
