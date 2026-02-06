572688d96184349b706e5db5ff33e0996c8e32dd
Sanskar Kumar <mindspirex@gmail.com>
2026-02-06 15:45:14 +0530

    done

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -4,15 +4,15 @@ import SideBar from "@/components/SideBar";
 import Canvas from "@/components/Canvas";
 import { ObjectsProvider } from "@/utility/useObjects";
 import { ZoomProvider } from "@/utility/useZoom";
+import ZoomControls from "@/components/ZoomControls";
 
 export default function Editor({ projectId }: { projectId: string }) {
   return (
     <ObjectsProvider projectId={projectId}>
       <ZoomProvider>
-        <div className="flex h-screen overflow-hidden">
-          <Canvas />
-          <SideBar />
-        </div>
+        <Canvas />
+        <SideBar />
+        <ZoomControls />
       </ZoomProvider>
     </ObjectsProvider>
   );
